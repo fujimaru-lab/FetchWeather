80560375be9818ae1e7c1688816b3498039ffe31
fujimaru-lab <yoshihiro.fujita@i-vinci.co.jp>
2019-08-17 11:26:24 +0900

    気象情報取得に使用する値の定数化
    WeatherAPIのURL、APPID、jsonファイルの出力先を定数化

---
+++ pkg/weather/WeatherConst.go
@@ -0,0 +1,12 @@
+package weather
+
+const (
+	// SourceURL is WeatherAPI URL
+	SourceURL = "http://api.openweathermap.org/data/2.5/weather"
+
+	// APPID for OpenWeather API
+	APPID = "43258cd3a93e0fb5f88294f3afdf1615"
+
+	// OutputDirPath 出力ファイル出力先ファイルパス
+	OutputDirPath = "../../resource/"
+)
